9ca283c59a8575fb34e4460b7800219b6c4406da
Sumeng Wang <suwang@ucsc.edu>
2019-07-26 12:27:10 -0700

    finish cropping our puzzle

--- puzzlescan.py
+++ puzzlescan.py
@@ -5,58 +5,54 @@ import math
 
 filename = 'puzzle1.png'
 img = cv2.imread(filename)
-""" gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray,2,3,0.04)
+outerBox = np.zeros(img.shape, np.uint8)
 
-#result is dilated for marking the corners, not important
-dst = cv2.dilate(dst,None) """
+gray = cv2.GaussianBlur(gray, (5,5), 0)
 
-# Threshold for an optimal value, it may vary depending on the image.
-img[32,404]=[0,0,255]
-img[404,417]=[0,0,255]
-img[43,32]=[0,0,255]
-img[417,43]=[0,0,255]
+outerBox = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 2)
 
-#A = np.where(dst>0.3*dst.max())
-#B = np.asarray(A).T
-#minx, miny, maxx, maxy = B[0][0], B[0][1],B[0][0], B[0][1]
-""" for pair in B:
-    if minx > pair[0]:
-        minx = pair[0]
-    if miny > pair[1]:
-        miny = pair[1]
-    if maxx < pair[0]:
-        maxx = pair[0]
-    if maxy < pair[1]:
-        maxy = pair[1]
+outerBox = cv2.bitwise_not(outerBox)
 
-upperleft = [x for x in B if x[0] == minx]
-bottomleft = [x for x in B if x[1] == miny]
-upperright = [x for x in B if x[1] == maxy]
-bottomright = [x for x in B if x[0] == maxx]
-print(upperleft,upperright,bottomleft,bottomright) """
+kernel = np.ones((3,3),np.uint8)
+outerBox = cv2.dilate(outerBox, kernel)
 
-rows,cols,ch = img.shape
+ret,thresh = cv2.threshold(outerBox,127,255,0)
+contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-pts1 = np.float32([[32,404],[404,417],[43,32]])
-pts2 = np.float32([[32,404],[404,404],[32,32]])
+#out = cv2.drawContours(img, contours, -1, (100, 0 ,100), 3)
 
-M = cv2.getRotationMatrix2D((cols/2,rows/2),-math.degrees(math.atan(13/372)),1)
-dst = cv2.warpAffine(img,M,(cols,rows))
-""" M = cv2.getPerspectiveTransform(pts1,pts2)
+maxContour = max(contours, key=cv2.contourArea)
 
+extLeft = tuple(maxContour[maxContour[:, :, 0].argmin()][0])
+extRight = tuple(maxContour[maxContour[:, :, 0].argmax()][0])
+extTop = tuple(maxContour[maxContour[:, :, 1].argmin()][0])
+extBot = tuple(maxContour[maxContour[:, :, 1].argmax()][0])
+
+# determine corner positions by quadrants
+midpoint = ((extLeft[0] + extRight[0])/2, (extTop[1]+extBot[1])/2)
+corners = [extLeft, extRight, extTop, extBot]
+cornerLT = (0, 0)
+cornerLB = (0, 0)
+cornerRT = (0, 0)
+cornerRB = (0, 0)
+for point in corners:
+    if point[0] < midpoint[0] and point[1] < midpoint[1]:
+        cornerLT = point
+    if point[0] < midpoint[0] and point[1] > midpoint[1]:
+        cornerLB = point
+    if point[0] > midpoint[0] and point[1] < midpoint[1]:
+        cornerRT = point
+    if point[0] > midpoint[0] and point[1] > midpoint[1]:
+        cornerRB = point
+
+# crop out puzzle and transform
+pts1 = np.float32([cornerLT,cornerRT,cornerLB,cornerRB])
+pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
 dst = cv2.warpPerspective(img,M,(300,300))
- """
-""" M = cv2.getAffineTransform(pts1,pts2)
 
-dst = cv2.warpAffine(img,M,(cols,rows))
- """
-plt.subplot(121),plt.imshow(img),plt.title('Input')
-plt.subplot(122),plt.imshow(dst),plt.title('Output')
-plt.show()
-
-""" cv2.imshow('dst',img)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows() """
+cv2.imshow("Image", dst)
+cv2.waitKey(0)
